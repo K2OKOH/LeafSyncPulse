f5e0bf2ea9103a0f42818c0b66b521fe108d311f
K2O <xmjtf@outlook.com>
2025-05-09 00:56:12 +0800

    [feature] modify the data show on html

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
-#include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <ESPmDNS.h>
+#include <Arduino.h> // 显式包含 Arduino.h 头文件
 
 const char* ssid = "Radio_Tower";
 const char* password = "xmjxmj777";
@@ -13,18 +13,28 @@ WebServer server(80);
 const int soilMoisturePin = 36;
 const int dryValue = 0; // Value when soil is dry
 const int wetValue = 4095; // Value when soil is wet
-// put function declarations here:
-int myFunction(int, int);
+
+// Function to read soil moisture percentage
+float readSoilMoisturePercentage() {
+  int soilMoistureValue = analogRead(soilMoisturePin);
+  // Map the raw value to a percentage with floating - point precision
+  float moisturePercentage = ((float)(soilMoistureValue - dryValue) / (float)(wetValue - dryValue)) * 100.0;
+  // Make sure the percentage is within the 0 - 100 range
+  return constrain(moisturePercentage, 0, 100);
+}
+
+// Function to handle requests to the root path
+void handleRoot();
+
+// Function to handle requests for non - existent pages
+void handleNotFound();
 
 void handleRoot() 
 {
-  // Read the value from the soil moisture sensor
-  int soilMoistureValue = analogRead(soilMoisturePin);
+  float moisturePercentage = readSoilMoisturePercentage();
 
-  // Map the raw value to a percentage
-  int moisturePercentage = map(soilMoistureValue, dryValue, wetValue, 0, 100);
-  // Make sure the percentage is within the 0 - 100 range
-  moisturePercentage = constrain(moisturePercentage, 0, 100);
+  char moisturePercentageStr[8];
+  dtostrf(moisturePercentage, 4, 2, moisturePercentageStr);
 
   // Build the HTTP response with auto - refresh meta tag
   String html = "<html><head>";
@@ -32,7 +42,7 @@ void handleRoot()
   html += "</head><body>";
   html += "<h1>Soil Moisture Sensor Reading</h1>";
   html += "<p>Soil Moisture Value: ";
-  html += moisturePercentage;
+  html += moisturePercentageStr;
   html += "%</p>";
   html += "</body></html>";
 
@@ -50,16 +60,19 @@ void handleNotFound() {
 }
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Initialize serial communication
+  Serial.begin(9600);
+  Serial.println("Starting setup...");
+
+  // Set pin modes
   pinMode(2, OUTPUT);
   pinMode(5, OUTPUT);
-  Serial.begin(9600);
   pinMode(4, INPUT_PULLUP);
-  // attachInterrupt(4, PinRaiseIntEvent, RISING);
+
+  // Set ADC resolution
   analogReadResolution(12);
 
-  //WiFi setup
+  // WiFi setup
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
@@ -68,51 +81,47 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  Serial.printf("WiFi connected success!!");
-  Serial.printf("IP: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("");
+  Serial.printf("WiFi connected success!! IP: %s\n", WiFi.localIP().toString().c_str());
 
+  // Start MDNS responder
   if (MDNS.begin("esp32"))
   {
-    Serial.printf("MDNS responder started");
+    Serial.println("MDNS responder started");
   }
 
+  // Define server routes
   server.on("/", handleRoot);
-
   server.on("/inline", []() {
     server.send(200, "text/plain","this work as well");
   });
-
   server.onNotFound(handleNotFound);
 
+  // Start the server
   server.begin();
-  Serial.printf("HTTP server started");
+  Serial.println("HTTP server started");
+
   delay(2000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(2, HIGH);
-  delay(900);
-  digitalWrite(2, LOW);
-  delay(100);
+  float moisturePercentage = readSoilMoisturePercentage();
   int adcValue = analogRead(36);
   float voltage = adcValue * 3.3 / 4095;
-  Serial.printf("ADC Value: %fV\r\n", voltage);
+  Serial.printf("ADC Value: %.2fV, Soil Moisture: %.2f%%\n", voltage, moisturePercentage);
+
+  // Check input on pin 4 and control output on pin 5
   if(digitalRead(4) == HIGH) {
-    Serial.printf("Water stop.\r\n");
+    Serial.println("Water stop.");
     digitalWrite(5, HIGH);
   }
   else {
-    Serial.printf("Water start.\r\n");
+    Serial.println("Water start.");
     digitalWrite(5, LOW);
   }
+
+  // Handle incoming client requests
   server.handleClient();
 
   delay(1000);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
